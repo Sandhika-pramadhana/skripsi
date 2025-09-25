0eacda477d7bd01ca4d35e2d58f8f021557a6593
Sandhika-pramadhana <sandhikapramadana@gmail.com>
2025-09-25 11:04:45 +0700

    fix error logout

--- src/actions/auth/auth.ts
+++ src/actions/auth/auth.ts
@@ -3,7 +3,7 @@ import { APIResponse, Credentials, LoginResponse, LogoutResponse} from "@/types/
 import { serverAction, ServerActionError } from "../action";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { EndpointLogin, EndpointLogout } from "@/types/api";
+import { EndpointLogin} from "@/types/api";
 
 export const LoginUser = serverAction(
   async (credentials: Credentials) => {
@@ -46,46 +46,3 @@ export const LoginUser = serverAction(
   "LOGIN_USER"
 );
 
-export const LogoutUser = serverAction(
-  async () => {
-    try {
-      const token = Cookies.get("token-auth");
-      
-      const res = await axios.post<LogoutResponse>(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL_1}/${EndpointLogout}`,
-        {},
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      
-      const { status, message, code } = res.data;
-      if (!status) {
-        throw new ServerActionError(message, code);
-      }
-
-      // Clear all authentication cookies
-      Cookies.remove("token-auth");
-      Cookies.remove("user_id");
-      Cookies.remove("name");
-      Cookies.remove("username");
-      Cookies.remove("roleId");
-
-      return { status, message, code };
-    } catch (error) {
-      // Clear cookies even if API call fails
-      Cookies.remove("token-auth");
-      Cookies.remove("user_id");
-      Cookies.remove("name");
-      Cookies.remove("username");
-      Cookies.remove("roleId");
-      
-      throw new ServerActionError("Terjadi kesalahan saat logout", "500");
-    }
-  },
-  "LOGOUT_USER"
-);
