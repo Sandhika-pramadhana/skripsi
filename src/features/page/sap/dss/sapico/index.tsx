d0c4c02b7a64e9cc47d431e3208026c69c9df17d
Sandhika-pramadhana <sandhikapramadana@gmail.com>
2025-12-17 05:26:25 +0700

    update 17/12/2025

--- src/features/page/sap/dss/sapico/index.tsx
+++ src/features/page/sap/dss/sapico/index.tsx
@@ -6,22 +6,27 @@ import Navbar from "@/features/core/components/navbar";
 import ListSapico from "./components/list-sapico";
 import { DatabaseIcon } from "lucide-react";
 
-export default function SapicoDssSection({ children }: { children: React.ReactNode }) {
+export default function SapicoDssSection({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <SidebarProvider>
       {/* Sidebar */}
       <AppSidebar />
 
       {/* Main Content */}
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 min-w-0">
         {/* Navbar */}
         <Navbar />
 
         {/* Main Section */}
-        <main className="flex-1 p-4">
+        <main className="flex-1 p-4 overflow-x-auto">
           <SidebarTrigger />
+
           {children}
-          
+
           {/* Title */}
           <div className="flex items-center justify-between mt-4">
             <div className="flex gap-2 items-center">
@@ -30,8 +35,10 @@ export default function SapicoDssSection({ children }: { children: React.ReactNo
             </div>
           </div>
 
-          {/* List Transaction Mandiri */}
-          <ListSapico />
+          {/*Table wrapper */}
+          <div className="w-full overflow-x-auto">
+            <ListSapico />
+          </div>
         </main>
       </div>
     </SidebarProvider>
