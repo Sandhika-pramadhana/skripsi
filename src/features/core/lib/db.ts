92ead08a5d6c56991f97cb730352f98aa0b6f4fc
Sandhika-pramadhana <sandhikapramadana@gmail.com>
2026-04-10 13:36:15 +0700

    update connection db to railway

--- src/features/core/lib/db.ts
+++ src/features/core/lib/db.ts
@@ -1,20 +1,23 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql, { Pool } from 'mysql2/promise';
 
-dotenv.config();
+/** Singleton MySQL Connection Pool */
+let pool: Pool | null = null;
 
+export function connectDB(): Pool {
+  if (!pool) {
+    pool = mysql.createPool({
+      host: process.env.NEXT_DB_HOST,
+      port: Number(process.env.NEXT_DB_PORT),
+      user: process.env.NEXT_DB_USER,
+      password: process.env.NEXT_DB_PASSWORD,
+      database: process.env.NEXT_DB_NAME,
+      connectionLimit: 10,
+      waitForConnections: true,
+      queueLimit: 0,
+    });
 
-/** MYSQL */
-export async function connectDB() {
-  return mysql.createPool({
-    host: process.env.NEXT_DB_HOST || '127.0.0.1',
-    port: Number(process.env.NEXT_DB_PORT) || 3306,
-    user: process.env.NEXT_DB_USER || 'root',
-    password: process.env.NEXT_DB_PASSWORD || '',
-    database: process.env.NEXT_DB_NAME || 'skripsi',
-    connectionLimit: 10,
-    waitForConnections: true,
-    queueLimit: 0,
-  });
+    console.log("✅ Database Connected to Railway");
+  }
+
+  return pool;
 }
-
